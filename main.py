8fc120df38e445ffaf681fb785a12f2d7f1b2544
Deniz Işık <denizisik2004@gmail.com>
2024-05-28 13:27:22 +0300

    feat: add unit vectors and perp lines

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 seedX = [0]
 seedY = [0]
 distances = [0]
+closest_seeds_index = []
 
 # Generate random points
 def generate_random(num=20):
@@ -30,7 +31,41 @@ print("seedX\tseedY\tdistances")
 for x, y, d in zip(seedX, seedY, distances):
   print(f"{x}\t{y}\t{d}")
 
+# Set graph axes
+plt.xlim(-15, 15)
+plt.ylim(-15, 15)
+
+def find_closest(): # Identify the closest point to the origin
+  distances_copy = distances.copy()
+  
+  closest_index = distances.index(min(distances_copy[1:]))
+  while closest_index in closest_seeds_index:
+    distances_copy[closest_index] = 100
+    closest_index = distances.index(min(distances_copy[1:]))
+
+  closest_x = seedX[closest_index]
+  closest_y = seedY[closest_index]
+  closest_seeds_index.append(closest_index)
+  return closest_x, closest_y
+
+for _ in range(3):
+  closest_x, closest_y = find_closest()
+  
+  # Draw a line from origin to Pc
+  plt.plot([0, closest_x], [0, closest_y], 'r--')
+
+  # Find the unit vector
+  magnitude = math.sqrt(closest_x**2 + closest_y**2)
+  unit_vector_x = -1*(closest_x / magnitude) # Reverse the direction so that it points to origin
+  unit_vector_y = -1*(closest_y / magnitude)
+  plt.arrow(closest_x, closest_y, -unit_vector_x, -unit_vector_y, head_width=0.3, head_length=0.5, fc='b', ec='b')
+
+  # Draw a perpendicular line to unit vector
+  perpendicular_x = -unit_vector_y
+  perpendicular_y = unit_vector_x
+  plt.plot([closest_x-perpendicular_x*100, closest_x+perpendicular_x*100], [closest_y-perpendicular_y*100, closest_y+perpendicular_y*100], 'g--')
+
 # Show plot
-plt.plot(0, 0, 'rx')
+plt.plot(0, 0, 'x', markersize=10, color='purple')
 plt.plot(seedX[1:], seedY[1:], "*")
 plt.show()
