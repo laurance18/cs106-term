55b25d4f7c6e745a3deccc5ceb6b94e8ccc0d5f3
Deniz Işık <denizisik2004@gmail.com>
2024-05-29 00:01:01 +0300

    feat: find intersection points and draw lines

--- main_v2.py
+++ main_v2.py
@@ -68,7 +68,7 @@ while True: # Will be broken when there are no more seeds to process (closest_x 
   print(f"Closest seed {len(previous_seeds)}: {closest_x}, {closest_y}")
 
   # SECTION: Draw line from closest point to the origin
-  plt.plot([0, closest_x], [0, closest_y], '--', color="purple")
+  # plt.plot([0, closest_x], [0, closest_y], '--', color="purple")
   midpoint_x = (0 + closest_x) / 2 # Record the midpoint of unit vector
   midpoint_y = (0 + closest_y) / 2
   unit_vector_mp.append((midpoint_x, midpoint_y))
@@ -77,14 +77,14 @@ while True: # Will be broken when there are no more seeds to process (closest_x 
   magnitude = math.sqrt(closest_x**2 + closest_y**2)
   unit_vector_x = -1*(closest_x / magnitude) # Multiply by -1 to flip the unit vector
   unit_vector_y = -1*(closest_y / magnitude)
-  plt.arrow(closest_x, closest_y, unit_vector_x, unit_vector_y, head_width=0.3, head_length=0.3, fc='red', ec='red')
+  # plt.arrow(closest_x, closest_y, unit_vector_x, unit_vector_y, head_width=0.3, head_length=0.3, fc='red', ec='red')
 
   # SECTION: Draw a line perpendicular to the unit vector
   perpendicular_x = -unit_vector_y
   perpendicular_y = unit_vector_x
   x_values = np.linspace(-15, 15, 100) # Seemingly infinite line (ray)
   y_values = [closest_y + perpendicular_y * (x - closest_x) / perpendicular_x for x in x_values]
-  plt.plot(x_values, y_values, 'g--')
+  # plt.plot(x_values, y_values, 'g--')
   perp_lines_slope.append(perpendicular_y / perpendicular_x) # Record the slope of the perpendicular line
 
   # SECTION: Draw unit vectors from closest to other seeds
@@ -105,15 +105,55 @@ while True: # Will be broken when there are no more seeds to process (closest_x 
     else:
       continue
 
-# SECTION: Voronoi diagram
+# SECTION: Find intersection of perpendicular lines
 
+def find_intersection(slope1, intercept1, slope2, intercept2):
+    # If the slopes are equal, the lines are parallel and have no intersection
+    if slope1 == slope2:
+        return None
+
+    # Calculate the x-coordinate of the intersection
+    x = (intercept2 - intercept1) / (slope1 - slope2)
+
+    # Substitute x into the equation of the first line to get the y-coordinate
+    y = slope1 * x + intercept1
+
+    return (x, y)
+
+perpendicular_lines = []
 for i in range(len(unit_vector_mp)): # Draw perpendicular lines from the midpoint of unit vectors
   slope = perp_lines_slope[i]
   x_intercept = unit_vector_mp[i][0]
   y_intercept = unit_vector_mp[i][1]
   x_values = np.linspace(-15, 15, 100)
   y_values = slope * (x_values - x_intercept) + y_intercept
+  start_x = x_values[0]
+  start_y = y_values[0]
+  end_x = x_values[-1]
+  end_y = y_values[-1]
+  perpendicular_lines.append((start_x, start_y, end_x, end_y))
   plt.plot(x_values, y_values, 'y-')
+
+intersections = []
+for i in range(len(perp_lines_slope)):
+    for j in range(i+1, len(perp_lines_slope)):
+        slope1 = perp_lines_slope[i]
+        intercept1 = unit_vector_mp[i][1] - slope1 * unit_vector_mp[i][0]
+        slope2 = perp_lines_slope[j]
+        intercept2 = unit_vector_mp[j][1] - slope2 * unit_vector_mp[j][0]
+        intersection = find_intersection(slope1, intercept1, slope2, intercept2)
+        if intersection is not None and intersection[0] >= -15 and intersection[0] <= 15 and intersection[1] >= -15 and intersection[1] <= 15:
+            intersections.append(intersection)
+plt.plot([x[0] for x in intersections], [x[1] for x in intersections], 'ro', markersize=5)
+
+intersection_lines = []
+for intersection in intersections:
+  # plt.plot([0, intersection[0]], [0, intersection[1]], '--', color="purple")
+  intersection_lines.append([0, intersection[0], 0, intersection[1]])
+
+# SECTION: Draw Voronoi cell
+
+
 
 print("Voronoi diagram completed.")
 
@@ -123,6 +163,6 @@ plt.ylim(-15, 15)
 
 plt.plot(0, 0, '*', markersize=8, color='orange')
 plt.plot(current_seedX[1:], current_seedY[1:], ".b", markersize=5)
-plt.plot(removed_seedsX[:], removed_seedsY[:], ".r", markersize=5)
+# plt.plot(removed_seedsX[:], removed_seedsY[:], ".r", markersize=5)
 plt.show()
 
