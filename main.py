a04ee67688e3b9283653dc66acbbf563132788f7
Deniz Işık <denizisik2004@gmail.com>
2024-05-28 11:13:38 +0300

    feat: add distance calculations to origin

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import random
 
 seedX = [0]
 seedY = [0]
+distances = [0]
 
 # Generate random points
-def generateRandom(num=20):
+def generate_random(num=20):
   for _ in range(num-1):
     r = random.uniform(2.5, 15)
     theta = math.radians(random.uniform(0, 360))
@@ -15,9 +16,19 @@ def generateRandom(num=20):
     y = r * math.sin(theta)
     seedX.append(round(x, 2))
     seedY.append(round(y, 2))
+generate_random()
 
+def calculate_distance(x1, y1, x2, y2):
+  return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
-generateRandom()
+# Calculate distances
+for i in range(1, len(seedX)):
+  distances.append(round(calculate_distance(0, 0, seedX[i], seedY[i]), 2))
+
+# Pretty print results
+print("seedX\tseedY\tdistances")
+for x, y, d in zip(seedX, seedY, distances):
+  print(f"{x}\t{y}\t{d}")
 
 # Show plot
 plt.scatter(0, 0, color='red')
