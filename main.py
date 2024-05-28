2656e5755e51555c6a800a56d87a6471ddc57f44
Deniz Işık <denizisik2004@gmail.com>
2024-05-28 14:09:33 +0300

    Necessary section commenting

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ seedY = [0]
 distances = [0]
 closest_seeds_index = []
 
-# Generate random points
+# SECTION: Random point generation
 def generate_random(num=20):
   for _ in range(num-1):
     r = random.uniform(2.5, 15)
@@ -19,10 +19,10 @@ def generate_random(num=20):
     seedY.append(round(y, 2))
 generate_random()
 
+# SECTION: Distance calculation
 def calculate_distance(x1, y1, x2, y2):
   return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
-# Calculate distances
 for i in range(1, len(seedX)):
   distances.append(round(calculate_distance(0, 0, seedX[i], seedY[i]), 2))
 
@@ -35,6 +35,7 @@ for x, y, d in zip(seedX, seedY, distances):
 plt.xlim(-15, 15)
 plt.ylim(-15, 15)
 
+# SECTION: Find the closest point to the origin
 def find_closest(): # Identify the closest point to the origin
   distances_copy = distances.copy()
   
@@ -48,6 +49,7 @@ def find_closest(): # Identify the closest point to the origin
   closest_seeds_index.append(closest_index)
   return closest_x, closest_y
 
+# SECTION: Main Loop
 for _ in range(3):
   closest_x, closest_y = find_closest()
   
@@ -67,7 +69,7 @@ for _ in range(3):
   y_values = [closest_y + perpendicular_y * (x - closest_x) / perpendicular_x for x in x_values]
   plt.plot(x_values, y_values, 'g--')
 
-# Show plot
+# Show final plot
 plt.plot(0, 0, 'x', markersize=10, color='purple')
 plt.plot(seedX[1:], seedY[1:], "*", markersize=5)
 plt.show()
