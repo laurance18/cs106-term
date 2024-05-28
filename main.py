2337c7406997e651f448d1385c15739074addb4f
Deniz Işık <denizisik2004@gmail.com>
2024-05-28 15:53:45 +0300

    fix: IndexError & finding closest

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ seedY = [0]
 final_seedX = [0]
 final_seedY = [0]
 distances = [0]
-closest_seeds_index = []
+closestX = []
+closestY = []
 
 # SECTION: Random point generation
 def generate_random(num=20):
   for _ in range(num-1):
     r = random.uniform(2.5, 15)
-    theta = math.radians(random.uniform(5, 360))
+    theta = math.radians(random.choice([x for x in range(361) if x not in [0, 90, 180, 360]]))
     x = r * math.cos(theta)
     y = r * math.sin(theta)
     seedX.append(round(x, 2))
@@ -23,7 +24,7 @@ generate_random()
 
 # SECTION: Distance calculation
 def calculate_distance(x1, y1, x2, y2):
-  return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+  return math.sqrt((abs(x2 - x1))**2 + (abs(y2 - y1))**2)
 
 for i in range(1, len(seedX)):
   distances.append(round(calculate_distance(0, 0, seedX[i], seedY[i]), 2))
@@ -39,21 +40,24 @@ plt.ylim(-15, 15)
 
 # SECTION: Find the closest point to the origin
 def find_closest(): # Identify the closest point to the origin
-  distances_copy = distances.copy()
-  
-  closest_index = distances_copy.index(min(distances_copy[1:]))
-  while closest_index in closest_seeds_index:
-    distances_copy[closest_index] = 100
-    closest_index = distances_copy.index(min(distances_copy[1:]))
+  distances_copy = distances[1:]
 
-  closest_x = seedX[closest_index]
-  closest_y = seedY[closest_index]
-  closest_seeds_index.append(closest_index)
+  closest_index = distances_copy.index(min(distances_copy))
+  while seedX[closest_index+1] in closestX:
+    distances_copy[closest_index] = math.inf
+    closest_index = np.argmin(distances_copy)
+  closest_x = seedX[closest_index+1]
+  closest_y = seedY[closest_index+1]
+
+  closestX.append(closest_x)
+  closestY.append(closest_y)
+
   return closest_x, closest_y
 
 # SECTION: Main Loop
-for _ in range(2):
+for _ in range(1):
   closest_x, closest_y = find_closest()
+  print(f"Closest point: ({closest_x}, {closest_y})")
   
   # Draw a line from origin to the closest point
   plt.plot([0, closest_x], [0, closest_y], 'r--')
@@ -74,7 +78,7 @@ for _ in range(2):
   # SECTION: Draw unit vectors from closest to other seeds
   seed_remove_index = []
   for i in range(1, len(seedX)):
-    if i not in closest_seeds_index:
+    if seedX[i] not in closestX and seedY[i] not in closestY:
       dx = seedX[i] - closest_x
       dy = seedY[i] - closest_y
       magnitude = math.sqrt(dx**2 + dy**2)
@@ -83,9 +87,8 @@ for _ in range(2):
 
       dot_product = unit_x_to * unit_vector_x + unit_y_to * unit_vector_y
       if dot_product >= 0:
-        print(f"Seed {i} is on the same side as the origin")
+        pass
       else:
-        print(f"Seed {i} is on the opposite side as the origin")
         seed_remove_index.append(i)
   for k in range(1, len(seedX)): # Remove seeds at opposite side of the origin
     if k in seed_remove_index:
